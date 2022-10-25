4edb5656e02bc3d32504d7ecbe9917d715de75c7
sohanemon <m.sohanemon@gmail.com>
2022-10-25 19:35:16 +0600

    styled with link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,13 @@
 import { list } from "postcss";
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const { pathname } = useLocation();
-  console.log(pathname);
   return (
     <>
       <nav className='bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
         <div className='container flex flex-wrap justify-between items-center mx-auto'>
-          <NavLink href='https://flowbite.com/' className='flex items-center'>
+          <Link href='https://flowbite.com/' className='flex items-center'>
             <img
               src='https://images.vexels.com/media/users/3/151566/isolated/lists/2e8706eaadcd2d297a542ac2385de370-15-minutes-clock-icon.png'
               className='mr-3 h-6 sm:h-9'
@@ -17,7 +16,7 @@ const Navbar = () => {
             <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
               15ML
             </span>
-          </NavLink>
+          </Link>
           <div className='flex md:order-2'>
             <button
               type='button'
@@ -55,17 +54,18 @@ const Navbar = () => {
             <ul className='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               {navItems.map((el) => (
                 <li key={el}>
-                  <NavLink
+                  <Link
                     to={`/${el === "home" ? "" : el}`}
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-indigo-500 underline" + navLinkClass
-                        : navLinkClass
+                    className={
+                      pathname === `/${el}` ||
+                      (pathname === "/" && el === "home")
+                        ? "text-indigo-500 underline" + LinkClass
+                        : LinkClass
                     }
                     aria-current='page'
                   >
                     {el}
-                  </NavLink>
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -78,6 +78,6 @@ const Navbar = () => {
 
 export default Navbar;
 
-const navLinkClass =
+const LinkClass =
   " capitalize block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-indigo-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 const navItems = ["home", "courses", "blogs", "about"];
